fc0a487f17c97c3f2b5d41a79a10a462dd004b28
ArdeshirV <e-job@protonmail.com>
2025-10-01 15:11:17 +0330

    Add constant color indexes

---
+++ colors.go
@@ -0,0 +1,25 @@
+package gocolor
+
+import "fmt"
+
+type Color byte
+
+const (
+	Normal      Color = 0
+	Bold        Color = 1
+	WhiteBold   Color = 1
+	Red         Color = 2
+	Teal        Color = 3
+	White       Color = 4
+	Blue        Color = 5
+	Green       Color = 6
+	Yellow      Color = 7
+	Magenta     Color = 8
+	RedBold     Color = 9
+	BlueBold    Color = 10
+	TealBold    Color = 11
+	GreenBold   Color = 12
+	YellowBold  Color = 13
+	MagentaBold Color = 14
+)
+
